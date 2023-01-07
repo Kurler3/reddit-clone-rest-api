8bbd38346ccc916958f11144d05905a08aab278e
Kurler3 <miguelbento2000@gmail.com>
2023-01-07 19:05:38 +0000

    Init!

---
+++ src/main/java/com/miguel/redditcloneapi/RedditcloneapiApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.redditcloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditcloneapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditcloneapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/redditcloneapi/RedditcloneapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.redditcloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditcloneapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
